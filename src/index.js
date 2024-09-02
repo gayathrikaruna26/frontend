ec2b9801a796ceea78f4e427bac73124bbe529bf
gayathrikaruna26 <gayathrikaruna89@gmail.com>
2024-09-02 15:56:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-import ParentComponent from './landing page/ParentComponent';
+import ParentComponent from './ParentComponent';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
